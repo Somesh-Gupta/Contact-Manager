4642791d4603a607f7af3e015f65c4c0b0de3dcf
Somesh-Gupta <someshjecrc@gmail.com>
2021-05-31 22:10:50 +0530

    Added RestTemplate to consume contacts-service REST API.

--- user-service/src/main/java/com/myapp/controller/UserController.java
+++ user-service/src/main/java/com/myapp/controller/UserController.java
@@ -1,16 +1,24 @@
 package com.myapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.myapp.entity.Contact;
 import com.myapp.entity.User;
 import com.myapp.service.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	// Constructor injection
 	// No need to annotate with @Autowired if only one constructor
@@ -22,6 +30,13 @@ public class UserController {
 
 	@GetMapping("/{userId}")
 	public User getUser(@PathVariable int userId) {
-		return userService.getUser(userId);
+
+		User user = userService.getUser(userId);
+		
+		List<Contact> contacts = this.restTemplate.getForObject("http://localhost:8002/contact/user/" + userId, List.class);
+		
+		user.setContacts(contacts);
+		
+		return user;
 	}
 }
